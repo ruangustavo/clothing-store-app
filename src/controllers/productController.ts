ae3f47ee9ff7acd64e048a9c7bb8864e63281580
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-03-23 17:26:22 -0300

    feat: add status code

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,31 +4,31 @@ import { Product } from "../models/product";
 export class ProductController {
   async getAllProducts(_req: express.Request, res: express.Response) {
     const products = await Product.findAll();
-    res.send(products);
+    res.status(200).send(products);
   }
 
   async getProductById(req: express.Request, res: express.Response) {
     const id = Number(req.params.id);
     const product = await Product.findById(id);
-    res.send(product);
+    res.status(200).send(product);
   }
 
   async createProduct(req: express.Request, res: express.Response) {
     const productToCreate = req.body;
     const createdProducts = await Product.create(productToCreate);
-    res.send(createdProducts);
+    res.status(201).send(createdProducts);
   }
 
   async updateProduct(req: express.Request, res: express.Response) {
     const id = Number(req.params.id);
     const productToUpdate = req.body;
     const updatedProduct = await Product.update(id, productToUpdate);
-    res.send(updatedProduct);
+    res.status(200).send(updatedProduct);
   }
 
   async deleteProduct(req: express.Request, res: express.Response) {
     const id = Number(req.params.id);
     await Product.delete(id);
-    res.send({ message: "Product deleted" });
+    res.status(200).send({ message: "Product deleted" });
   }
 }
